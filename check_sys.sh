a591ff0a591200b57065aae7c9d608bc06d763fe
Andy <andy8805232@gmail.com>
2025-11-21 20:44:05 +0800

    1w

---
+++ check_sys.sh
@@ -0,0 +1,9 @@
+echo "=== CPU 信息 ===" && lscpu | grep 'Model name'
+echo
+echo "=== 内存使用 ===" && free -h
+echo
+echo "=== 磁盘使用 ===" && df -h
+echo
+echo "=== 网络信息 ===" && hostname -I
+echo
+echo "=== 系统负载 ===" && uptime
